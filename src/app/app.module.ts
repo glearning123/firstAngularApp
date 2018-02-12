d1467a655ac508a88c4530dfcb0400ed0f7f98e1
glearning123 <killabll00d@gmail.com>
2018-02-12 19:20:08 +1000

    Services (not done)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,24 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 import { HeroesComponent } from './heroes/heroes.component';
-
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppNavbarComponent,
     HeroesComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
     FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
